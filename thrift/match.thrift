917e0cdd8738fc6cbb9386407b3a39b8073c8d78
xxx <xxx>
2025-08-10 12:19:45 +0800

    update match.thrift and save.thrift

--- thrift/match.thrift
+++ thrift/match.thrift
@@ -1,3 +1,5 @@
+# thrift的IDL
+
 namespace cpp match_service
 
 struct User {
@@ -7,7 +9,17 @@ struct User {
 }
 
 service Match {
+    /**
+     * user: 添加的用户信息
+     * info: 附加信息
+     * 在匹配池中添加一个名用户
+     */
     i32 add_user(1: User user, 2: string info),
 
+    /**
+     * user: 删除的用户信息
+     * info: 附加信息
+     * 从匹配池中删除一名用户
+     */
     i32 remove_user(1: User user, 2: string info),
 }
